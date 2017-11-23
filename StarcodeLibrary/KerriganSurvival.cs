a7de0747f0fa895ebc409c19cdf867666a0c18e9
RaiNote <Timothy.Lutz@gmx.de>
2017-11-23 21:44:19 +0100

    blub

--- StarcodeLibrary/KerriganSurvival.cs
+++ StarcodeLibrary/KerriganSurvival.cs
@@ -11,16 +11,19 @@ namespace StarcodeLibrary
         public class ArrayStruct
         {
             public List<float> LvArray = new List<float>(ArrayListMaxLength + 1);
+            public int LvSize;
         }
 
         public class ArrayStructString
         {
             public List<string> LvArray = new List<string>(ArrayListMaxLength + 1);
+            public int LvSize;
         }
 
         public class ArrayStructText
         {
             public List<string> LvArray = new List<string>(ArrayListMaxLength + 1);
+            public int LvSize;
         }
 
         public List<ArrayStruct> ArrayList = new List<ArrayStruct>(201);
@@ -83,13 +86,9 @@ namespace StarcodeLibrary
 
         public int DecryptArrayList(string lpInput, int lpWhichList, int lpLength)
         {
-            // Automatic Variable Declarations
             const int autoCc47Fd97Ai = 1;
 
-            // Variable Initialization
-
-            // Implementation
-            gf_ArrayListClear(lpWhichList);
+            ArrayListClear(lpWhichList);
             if (ValidateString(lpInput, 2))
             {
                 string lvTempString = RemoveHashfromString(lpInput, 2);
@@ -109,8 +108,29 @@ namespace StarcodeLibrary
 
         public void ArrayListAdd(int lpWhichList, float lpValue) //gf_ArrayListAdd
         {
-            ArrayList[lpWhichList].LvArray[ArrayList[lpWhichList].LvArray.Count] = lpValue;
-            //ArrayList[lpWhichList].LvArray.Count = (ArrayList[lpWhichList].LvArray.Count + 1);
+            ArrayList[lpWhichList].LvArray[ArrayList[lpWhichList].LvSize] = lpValue;
+            ArrayList[lpWhichList].LvSize = (ArrayList[lpWhichList].LvSize + 1);
         }
+
+        public void ArrayListRemove(int lpWhichList, float lpIndex) //gf_ArrayListRemove
+        {
+            const int auto18Cb8824Ai = 1;
+
+            if ((lpIndex >= ArrayList[lpWhichList].LvSize)) { throw new Exception("#1"); }
+
+            int auto18Cb8824Ae = (ArrayList[lpWhichList].LvSize - 1);
+            int lvI = (int)lpIndex;
+            for (; ((auto18Cb8824Ai >= 0 && lvI <= auto18Cb8824Ae) || (auto18Cb8824Ai < 0 && lvI >= auto18Cb8824Ae)); lvI += auto18Cb8824Ai)
+            {
+                ArrayList[lpWhichList].LvArray[lvI] = ArrayList[lpWhichList].LvArray[(lvI + 1)];
+            }
+            ArrayList[lpWhichList].LvSize = (ArrayList[lpWhichList].LvSize - 1);
+        }
+
+        public void ArrayListClear(int lpWhichList) //gf_ArrayListClear
+            => ArrayList[lpWhichList].LvSize = 0;
+
+        public void ArrayListCut(int lpWhichList, int lpSize) //gf_ArrayListCut
+            => ArrayList[lpWhichList].LvSize = lpSize;
     }
 }
